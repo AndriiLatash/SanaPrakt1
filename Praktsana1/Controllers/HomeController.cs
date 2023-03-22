5e905bb0f60d9dfa2a9d6ba5a90bad379cb038fc
Andrii <latasandrej@gmail.com>
2023-03-22 23:43:55 +0200

    first commit

---
+++ Praktsana1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Praktsana1.Models;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace Praktsana1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+
+        public IActionResult MyAction(string myText)
+        {
+            ViewBag.MyText = myText;
+            return View("Index");
+        }
+    }
+}
